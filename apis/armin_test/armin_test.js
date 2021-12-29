1fba2732080fc37c9bda8e18d02e982f9f71691a
Armin Goodarzi <armingodarzi1380@gmail.com>
2021-12-29 10:53:00 +0330

    Update armin_test.js

--- apis/armin_test/armin_test.js
+++ apis/armin_test/armin_test.js
@@ -8,7 +8,7 @@ exports.handler = async function(event, context) {
   "multiValueHeaders": { 
     "X-Custom-Header": ["My value", "My other value"],
   },
-  "body": "from " + event.body.name + " received body is " + JSON.stringify(event.body) + " received headers: "
+  "body": "2) from " + event.body.name + " received body is " + JSON.stringify(event.body) + " received headers: "
    + JSON.stringify(event.headers)  + " request method and path: " + event.method + ", " + event.path + " and one example header: " + event.headers.Connection
 }
 return response
